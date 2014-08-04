9861a6831489c30a3bad40ad030c1afc9e306e33
robin ma <anhmzj@gmail.com>
2014-08-04 19:18:06 +0800

    recreate html and css

---
+++ src/js/videoplayer.js
@@ -0,0 +1,14 @@
+/*!
+ *vedioPlayer.js
+ *HTML% <video> player
+ *
+ *chreate a javascript Object that mimics html5 Media API
+ *can play MP4,Ogg,WebM
+ *
+ *License:MIT
+ *
+ */
+ (function (argument) {
+     // body...
+
+ })();
