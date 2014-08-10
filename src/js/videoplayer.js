099e059ef9d960630e7f35af963052897f0fc3b7
robinma <ahmzj@163.com>
2014-08-11 00:31:29 +0800

    add mouse holder event

--- src/js/videoplayer.js
+++ src/js/videoplayer.js
@@ -63,7 +63,6 @@
         init: function() {
             this.render();
             this.renderSource();
-
             this.events()
         },
         render: function() {
@@ -102,14 +101,6 @@
                 console.log('loadstart', __.index)
             }).on('ended', function() {
                 console.log('play ended')
-            })
-
-            this.$video.on('pause', function() {
-                __.isPlay = false;
-            }).on('play',function() {
-                __.isPlay = true;
-            }).on('ended', function() {
-                __.isPlay = false;
             })
 
         }
@@ -186,13 +177,21 @@
             if (params.autoPlay) {
                 this.$video[0].play();
             }
+            //set is play value
+            this.$video.on('pause', function() {
+                __.isPlay = false;
+            }).on('play', function() {
+                __.isPlay = true;
+            }).on('ended', function() {
+                __.isPlay = false;
+            })
+
             this.$video.on('error', function() {
                 __._deadlyError();
             }).on('loadedmetadata', function() {
 
             });
             this.controlsInit();
-
         }
     });
 
@@ -200,13 +199,16 @@
 
     //controls bar
     $.extend(Video.prototype, {
-        _showControl: true,
+        _mouseOnControl: false,
         $_control: '',
         $_membrane: '',
         $_minprogress: '',
         controlsInit: function() {
+            //insert play control mask
             this.insertMembrane();
+            //insert control warp
             this.renderControls();
+            //insert progress node
             this.renderDefProgress()
             this.playRender();
             this.nextRender()
@@ -238,20 +240,24 @@
             var __ = this;
             this.$_membrane.on('click', function() {
                 __._play_on_off();
+            }).on('mousemove',function(){
+                __._controlBar_moveshow();
             });
 
             this.$video.on('play', function() {
                 __._controlBar_delayhide()
             }).on('pause', function() {
                 __._controlBar_show()
-            }).on('ended',function(){
+            }).on('ended', function() {
                 __._controlBar_show();
             });
-            
-            this.$_control.on('mouseover',function(){
-
-            }).on('mouseout',function(){
-
+            //set on mouse over control bar flag value
+            this.$_control.on('mouseover', function() {
+                __._mouseOnControl=true;
+                clearTimeout(__._timer)
+            }).on('mouseleave', function() {
+                __._mouseOnControl=false;
+                __._controlBar_delayhide();
             });
         },
         _controlBar_show: function() {
@@ -260,14 +266,31 @@
         _controlBar_hide: function() {
             this.$el.addClass('yvp_container_hide');
         },
-        _timer:'',
+        _timer: '',
+        _moveshow:'',
         _controlBar_delayhide: function() {
             var __ = this;
-            if(!this.isPlay) return;
+            if (!this.isPlay) return;
+            if(this._mouseOnControl) return;
             clearTimeout(__._timer)
             __._timer = setTimeout(function() {
                 __._controlBar_hide();
-            }, 1500);
+            }, 3000);
+        },
+        _controlBar_moveshow:function(){
+            var __=this;
+            if(__._moveshow > 0){
+                clearTimeout(__._moveshow);
+                clearTimeout(__._timer);
+                __._moveshow = setTimeout(function(){
+                    __._controlBar_delayhide();
+                    __._moveshow = '';
+                },0);
+
+                return;
+            }
+            this._controlBar_show();
+            this._moveshow=1;
         }
     });
 
@@ -872,7 +895,6 @@
 
         },
         enterFullScreen: function() {
-            console.log(this.video.$video)
             this._insertMask();
             this.video.$el.appendTo(this.$warp)
             this._setWarpInfo()
